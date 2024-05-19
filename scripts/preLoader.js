ff8f3c86061eeaddea2e91094b18a3b50a17e130
AiaPrinci <davidjimsheleishvili17@gmail.com>
2024-05-19 23:07:07 +0400

    edited

--- scripts/preLoader.js
+++ scripts/preLoader.js
@@ -1,11 +1,13 @@
-let parent = document.getElementById('parent');
+let scrollEd = document.getElementById('scroll-container');
 
 window.addEventListener("load", () => {
     const loader = document.querySelector(".loader");
   
     loader.classList.add("loader--hidden");
+
+    scrollEd.style.display = 'flex';
   
     loader.addEventListener("transitionend", () => {
       document.body.removeChild(loader);
     });
-  });
+});
