77a4f4756de491ddf7b0e47ecf69f5df0ede664a
AiaPrinci <davidjimsheleishvili17@gmail.com>
2024-05-28 01:25:44 +0400

    eddited

---
+++ scripts/onScroll.js
@@ -0,0 +1,17 @@
+// const  scroll = new IntersectionObserver((effect) => {
+//     effect.forEach((set)=>{
+//         if(set.isIntersecting){
+//             set.target.classList.add('show');
+//         }else{
+//             set.target.classList.remove('show');
+//         };
+//     });
+// });
+
+// let scrollElement = document.querySelectorAll('section');
+
+// scrollElement.forEach((item) => {
+//     console.log(item);
+// })
+
+// scrollElement.forEach((el) => scroll.observe(el));
